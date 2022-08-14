265024a9e1e66add22fa4490e404c31a2e8521f8
veiv <veiv@mail.ru>
2022-08-14 03:20:18 +0300

    add SolutionTask42

---
+++ SolutionTask42/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача №42
+// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// Например:
+// 45 -> 101101
+// 3 -> 11
+
+Console.WriteLine(Convert.ToString(int.Parse(Console.ReadLine()),2));
+
+int number = int.Parse(Console.ReadLine());
+string outline = (Convert.ToString(number,16));
+Console.WriteLine(outline);
