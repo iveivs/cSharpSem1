4fad1c067b09a48e317dde1b80743abe773143f0
veiv <veiv@mail.ru>
2022-08-17 00:41:18 +0300

    add SolutionTask48

--- SolutionTask48/Program.cs
+++ SolutionTask48/Program.cs
@@ -6,7 +6,7 @@
 // 1 2 3 4
 // 2 3 4 5
 
-// Метод для заполнения двумерного массива
+// Метод для заполнения двумерного массива cлучайными числами
 int[,] FillTwoDimArray(int countRow, int countColumn)
 {
     System.Random numberSintezator = new System.Random();
