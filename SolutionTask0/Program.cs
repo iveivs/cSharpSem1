7c1d3028a5f552d74ecb12458cc983b5ba362b04
veiv <veiv@mail.ru>
2022-08-23 14:29:02 +0300

    add SolutionTask 0

--- SolutionTask0/Program.cs
+++ SolutionTask0/Program.cs
@@ -1,4 +1,10 @@
-﻿string? inputLine = Console.ReadLine();
+﻿// Задача №0
+// Напишите программу, которая на вход принимает
+// число и выдаёт его квадрат (число умноженное на
+// само себя).
+// Например: 4 -> 16 -3 -> 9 -7 -> 49 
+
+string? inputLine = Console.ReadLine();
 
 if(inputLine != null)
 {
