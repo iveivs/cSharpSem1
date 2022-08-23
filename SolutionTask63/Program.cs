2eb7c3068c80b6fd734c81b47bc92d363da497d7
veiv <veiv@mail.ru>
2022-08-23 03:04:04 +0300

    add SolutionTask 63

---
+++ SolutionTask63/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 63.  Задайте значение N. Напишите программу, которая выдаёт все натуральные числа в промежутке от 1 до N.
+// Например:
+// N = 5 -> "5, 4, 3, 2, 1"
+
+
+Console.Clear();
+
+int ReadData()
+{
+    Console.WriteLine("Задайте число");
+    return int.Parse(Console.ReadLine());
+}
+
+int NaturalNumberPrinter(int num)
+{
+    // Делаем остановку нашего метода
+    if(num == 2) return 1;
+    else
+    {
+        num--;
+        Console.WriteLine(num);
+        Console.Write(NaturalNumberPrinter(num) + " ");
+    }
+    return num;
+}
+
+int inputNumber = ReadData();
+NaturalNumberPrinter(inputNumber + 2);
