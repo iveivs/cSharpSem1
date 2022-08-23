343ed904f21976251bd4e981f034f2bb6af86a7e
veiv <veiv@mail.ru>
2022-08-23 14:32:03 +0300

    add SolutionTask 1

--- SolutionTask1/Program.cs
+++ SolutionTask1/Program.cs
@@ -1,4 +1,10 @@
-﻿string? inputLineOne = Console.ReadLine();
+﻿// Задача 1        Напишите программу, которая на вход принимает
+//                 два числа и проверяет, является ли первое число
+//                 квадратом второго. Например:
+//                 a = 25; b = 5 -> да
+//                 a = 2 b = 10 -> нет
+
+string? inputLineOne = Console.ReadLine();
 string? inputLineTwo = Console.ReadLine();
 
 if(inputLineOne != null && inputLineTwo != null)
