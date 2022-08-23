1667f0ca806c72fea5d5341d4addf62ffc16b3c9
veiv <veiv@mail.ru>
2022-08-23 14:28:42 +0300

    add SolutionTask 67

---
+++ SolutionTask67/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 67   Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр
+//             452 -> 11
+//             63  -> 9
+
+Console.Clear();
+
+int number =  ReadData("Задайте число: ");
+
+Console.WriteLine(Sum(number));
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine());
+}
+
+int Sum(int num)
+{
+    if(num > 0)
+    return num % 10 + Sum(num/10);
+    else
+    {
+        return 0;
+    }
+}
